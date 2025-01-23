894e3a10b50f487e0f5d0e80633bc3759195525d
Tristan Level <216LevelUp@gmail.com>
2025-01-23 17:14:25 -0500

    color

--- Program.cs
+++ Program.cs
@@ -1,21 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-var brick = new Brick()
-{
-    Width = 1.2,
-    Height = 3.4,
-    Thickness = 5.6,
-};
+// Brick ConstructBrick(double width, double height, double thickness)
+// {
+//     return new Brick()
+//     {
+//         Width = width,
+//         Height = height,
+//         Thickness = thickness,
+//     };
+// }
 
-brick.GetVolume();
+var brick = new Brick(1.2, 3.4, 5.6, "red");
+var brick2 = new Brick(6.5, 4.3, 2.1, "grey");
 
-var brick2 = new Brick()
-{
-    Width = 9.8,
-    Height = 7.6,
-    Thickness = 5.4,
-};
+brick.PrintColor();
+brick2.PrintColor();
 
-Console.WriteLine($"The volume of brick 1 is {brick.GetVolume()}!");
-Console.WriteLine($"The volume of brick 1 is {brick2.GetVolume()}!");
+// var brick = new Brick()
+// {
+//     Width = 1.2,
+//     Height = 3.4,
+//     Thickness = 5.6,
+// };
+
+//brick.GetVolume();
+
+// var brick2 = new Brick()
+// {
+//     Width = 9.8,
+//     Height = 7.6,
+//     Thickness = 5.4,
+// };
+
+//Console.WriteLine($"The color of brick 1 is {brick.PrintColor}!");
+//Console.WriteLine($"The color of brick 2 is {brick2.PrintColor}!");
--- brick.cs
+++ brick.cs
@@ -10,4 +10,24 @@ public class Brick
         var width = 3;
         return Width * Height * Thickness;
     }
+
+    public Brick(double width, double height, double thickness, string color)
+{
+    
+    
+        Width = width;
+        Height = height;
+        Thickness = thickness;
+        Color = color;
+    
+}
+public void SetColor(string color)
+{
+    Color = color;
+}
+
+public void PrintColor()
+{
+    Console.WriteLine($"The color of this brick is {Color}");
+}
 }
